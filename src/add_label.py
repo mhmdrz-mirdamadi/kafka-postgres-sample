aa5a94082a9d94447c8620fcd54a19b981ea469b
Mohammadreza Mirdamadiyan <smrm6789@gmail.com>
2024-04-03 18:46:42 +0330

    add random label

--- src/add_label.py
+++ src/add_label.py
@@ -1,12 +1,18 @@
 import json 
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+from wonderwords import RandomWord
 
 if __name__ == '__main__':
-    print("Connecting to consumer ...")
+    r = RandomWord()
+
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     consumer = KafkaConsumer(
         'users_info',
         bootstrap_servers=['broker:29092'],
         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
     for message in consumer:
-        print(f"{message.value}")
+        res = message.value
+        res['label'] = r.word(include_parts_of_speech=["nouns", "adjectives"])
+        producer.send('users_label', json.dumps(res).encode('utf-8'))
+        print(f"{res}")
--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
 exec python3 get_data.py &
 exec python3 add_label.py
